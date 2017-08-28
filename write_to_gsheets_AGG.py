6740d63f297dd4d16750ae702d68c7db5dc8b369
mefryar <mfryar.hks@gmail.com>
2017-08-28 16:09:39 -0400

    Consistently use spaces instead of tabs for indentation.

--- write_to_gsheets_AGG.py
+++ write_to_gsheets_AGG.py
@@ -26,9 +26,9 @@ course_id = "course-v1:epodx+BCURE-AGG+2016_v1"
 
 def write_to_sheet():
     """
-	Get learner profile information for course_id from epodx
-	"""
-	# The list of fields you've requested. Leave this parameter off to see the
+    Get learner profile information for course_id from epodx
+    """
+    # The list of fields you've requested. Leave this parameter off to see the
     # full list of fields.
     fields = ','.join(
                     ["user_id", "username", "name", "email", "language",
@@ -40,14 +40,14 @@ def write_to_sheet():
     learner_profile_report_url = "http://localhost:18100/api/v0/learners/"
 
     headers = {
-			"Authorization":"Token {}".format(hks_secret_token),
-			"Accept": "text/csv",
-	}
+            "Authorization":"Token {}".format(hks_secret_token),
+            "Accept": "text/csv",
+    }
 
     params = {
-			"course_id": course_id,
-			"fields": fields,
-	}
+            "course_id": course_id,
+            "fields": fields,
+    }
     with requests.Session() as s:
         download = s.get(learner_profile_report_url, headers=headers,
                          params=params)
@@ -56,30 +56,30 @@ def write_to_sheet():
 
         cr = csv.reader(decoded_content.splitlines(), delimiter=',')
         learner_profiles = list(cr)
-	"""
-	Get problem response information for course_id from epodx
-	"""
-	problem_api_url = ("http://localhost:18100/api/v0/courses/"
-						"{}/reports/problem_response".format(course_id))
-	headers = {"Authorization":"Token {}".format(hks_secret_token)}
-	problem_data = requests.get(problem_api_url, headers=headers).json()
-	problem_download_url = problem_data['download_url']
-	# Download the CSV from download_url
-	with requests.Session() as s:
-	    download = s.get(problem_download_url)
+    """
+    Get problem response information for course_id from epodx
+    """
+    problem_api_url = ("http://localhost:18100/api/v0/courses/"
+                        "{}/reports/problem_response".format(course_id))
+    headers = {"Authorization":"Token {}".format(hks_secret_token)}
+    problem_data = requests.get(problem_api_url, headers=headers).json()
+    problem_download_url = problem_data['download_url']
+    # Download the CSV from download_url
+    with requests.Session() as s:
+        download = s.get(problem_download_url)
 
-	decoded_content = download.content.decode('ascii', 'ignore')
+    decoded_content = download.content.decode('ascii', 'ignore')
 
-	cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-	problem_responses = list(cr)
-	"""
-	Building on quickstart template to try to write to Google Sheets.
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    problem_responses = list(cr)
+    """
+    Building on quickstart template to try to write to Google Sheets.
 
-	Creates a Sheets API service object and writes learner profile
+    Creates a Sheets API service object and writes learner profile
     information to Master Sheet. To open Master sheet go to:
     https://docs.google.com/spreadsheets/d/<spreasheetId>/edit
     plugging in value of <spreadsheetId> below.
-	"""
+    """
     http = credentials.authorize(httplib2.Http())
     discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
                     'version=v4')
@@ -101,9 +101,9 @@ def write_to_sheet():
     ]
     body = {'valueInputOption': 'RAW', 'data': data}
     result = service.spreadsheets().values().batchUpdate(
-		spreadsheetId=spreadsheetId, body=body).execute()
+        spreadsheetId=spreadsheetId, body=body).execute()
 
 
 if __name__ == '__main__':
-	credentials = get_credentials.get_credentials()
-	write_to_sheet()
+    credentials = get_credentials.get_credentials()
+    write_to_sheet()
