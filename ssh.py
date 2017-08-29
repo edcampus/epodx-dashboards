6992b55bdcfb0e3dafd854ec250ef80498ddeaf2
mefryar <mfryar.hks@gmail.com>
2017-08-29 16:26:57 -0400

    Fix ssh arguments and run ssh subprocess

--- ssh.py
+++ ssh.py
@@ -22,16 +22,15 @@ os.chdir(epodx_dir)
 # -f "fork into background", -F "use configuration file"
 # -o ExistOnForwardFailure=yes "wait until connection and port
 #   forwardings are set up before placing in background"
-# sleep 10 "give python script 60 seconds to start using tunnel and
+# sleep 10 "give python script 10 seconds to start using tunnel and
 #   close tunnel after python script stops using it"
 # Ref 1: https://www.g-loaded.eu/2006/11/24/auto-closing-ssh-tunnels/
 # Ref 2: https://gist.github.com/scy/6781836
 
 config = "-F ./ssh-config epodx-analytics-api"
 option = "-o ExitOnForwardFailure=yes"
+ssh = "ssh -f {} {} sleep 10".format(config, option)
+subprocess.run(ssh)
 
-ssh = subprocess.run("ssh -f config option sleep 60")
-
-# Change to directory containing script to write to Google Sheets
-os.chdir("C:/Users/mifryar/Documents/epodx-dashboards")
+# Write to Google Sheets
 write_to_sheet()
